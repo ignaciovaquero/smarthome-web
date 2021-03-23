33bf35dc3a7438f72031d92b3ef30a7e29a83618
Ignacio Vaquero Guisasola <ivaquero@twilio.com>
2021-03-23 09:16:08 +0100

    Initial commit

---
+++ public/config.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+const config = (() => ({
+  TWILIO_WORLDS_BATTLESTAR_URL: 'https://battlestar.corp.twilio.com/v3/Worlds',
+}))();
---
+++ src/components/SmartHomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="../assets/smarthome-logo.png" />
+    <h1>SmartHome</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SmartHomePage',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
